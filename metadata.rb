b6334f9ffffef91b051b483fc31205256242419c
Ed Overton <e.overton@gmail.com>
2020-07-26 16:16:15 -0400

    bump version

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'you1@example.com'
 chef_version     '>= 13.9'
 license          'Apache-2.0'
 description      'Installs/Configures chefsrv'
-version          '1.3.0'
+version          '1.4.0'
 
 supports 'redhat'
 supports 'centos'
